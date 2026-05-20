fdc7b657ada777db7a4d3b7afbb8006065342a6c
Krishlj <krishlj@gmail.com>
2026-05-20 16:55:58 +0530

    Add Prometheus metrics

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const client = require('prom-client');
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics();
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
+});
+
 const express = require('express');
 
 const app = express();
